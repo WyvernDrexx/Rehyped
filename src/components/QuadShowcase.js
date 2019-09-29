2bebc7a28fef4cf311b2db87753244d3b5b6d928
Roscrach <lilaprashadsharma@gmail.com>
2019-09-29 18:43:42 +0530

    updated home with 500x600.jpeg image

--- src/components/QuadShowcase.js
+++ src/components/QuadShowcase.js
@@ -8,17 +8,17 @@ const QuadShowcase = props => {
       <div className="d-flex justify-content-center">
         <Row className="quadshowcase">
           <Col className="primary-image pl-0" xs={9}>
-            <img className="h-100 w-100" src="/imgs/square.jpg" />
+            <img className="h-100 w-100" src="/imgs/500x600.jpeg" />
           </Col>
           <Col>
             <Row className="secondary-image">
-              <img className="h-100 w-100" src="/imgs/square.jpg" />
+              <img className="h-100 w-100" src="/imgs/500x600.jpeg" />
             </Row>
             <Row className="secondary-image">
-              <img className="h-100 w-100" src="/imgs/square.jpg" />
+              <img className="h-100 w-100" src="/imgs/500x600.jpeg" />
             </Row>
             <Row className="secondary-image">
-              <img className="h-100 w-100" src="/imgs/square.jpg" />
+              <img className="h-100 w-100" src="/imgs/500x600.jpeg" />
             </Row>
           </Col>
         </Row>
