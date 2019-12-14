edd64914ef9ed5791933a34d5e125d6cf777e2a3
Roscrach <lilaprashadsharma@gmail.com>
2019-12-14 21:46:44 +0530

    onLoad > ListItem: Result: loading smooth and fast

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -3,44 +3,8 @@ import { Row, Col, Spinner } from "react-bootstrap";
 import { fetchProducts } from "../../actions";
 import { useDispatch, useSelector } from "react-redux";
 import history from "../../history";
-
+import ListItem from './ListItem'
 import "./ProductsList.scss";
-import ImgLoader from "../ImgLoader";
-
-const renderList = list => {
-  return list.map((item, index) => {
-    return (
-      <Col
-        onClick={_ => history.push(`/products/${item.productId}`)}
-        key={index}
-        xs={6}
-        sm={4}
-        lg={3}
-        className="product-item mb-5"
-      >
-        <Row>
-          <ImgLoader alt="This is null" src={item.image} />
-          {/* <img alt="This is null" src={item.image} /> */}
-        </Row>
-        <Row>
-          <div className="mt-4">
-            <p className="sub-header font-weight-bold text-left">
-              {item.name.length > 23
-                ? item.name.slice(0, 23) + "..."
-                : item.name}
-            </p>
-            <p className="mt-4 mr-4">
-              <span className="strikethrough sub-header mr-4">
-                ${item.price}
-              </span>
-              <span className="sub-header">$1500</span>
-            </p>
-          </div>
-        </Row>
-      </Col>
-    );
-  });
-};
 
 const ProductsList = props => {
   const products = useSelector(state => state.products);
@@ -60,6 +24,12 @@ const ProductsList = props => {
       }
     };
   }, []);
+
+  const renderList = (list = []) => {
+    return list.map((item, index) => {
+      return <ListItem item={item} index={index} />
+    })
+  }
 
   if (Object.values(products).length === 0) {
     return (
