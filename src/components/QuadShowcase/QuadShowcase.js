7e4da0db9c804a10ba06791bb6ed95c7c605f19f
Roscrach <lilaprashadsharma@gmail.com>
2019-12-15 09:47:39 +0530

    spinner added on product image load

--- src/components/QuadShowcase/QuadShowcase.js
+++ src/components/QuadShowcase/QuadShowcase.js
@@ -1,23 +1,34 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Row, Spinner } from "react-bootstrap";
 import "./QuadShowcase.scss";
 
 const QuadShowcase = props => {
-  if(props.image){
+  const [divIsLoaded, setDivIsLoaded] = useState(false);
+  const divIsMounted = _ => {
+    setDivIsLoaded(true);
+  };
+
+  if (props.image) {
     return (
       <>
-        <Row className="quadshowcase">
+        {divIsLoaded ? null : (
+          <Spinner className="absolute-center" animation="border" />
+        )}
+        <Row
+          onLoad={divIsMounted}
+          className={`quadshowcase ${divIsLoaded ? "" : "visibility-hidden"}`}
+        >
           <Col lg={true} className="primary-image px-0">
             <img alt="This is null" className="" src={props.image} />
           </Col>
-          <Col md={6} lg={4}className="sm-0 px-0 pl-md-2">
-            <div  className="secondary-image">
+          <Col md={6} lg={4} className="sm-0 px-0 pl-md-2">
+            <div className="secondary-image">
               <img alt="This is null" className="" src={props.image} />
             </div>
-            <div  className="secondary-image">
+            <div className="secondary-image">
               <img alt="This is null" className="" src={props.image} />
             </div>
-            <div  className="secondary-image">
+            <div className="secondary-image">
               <img alt="This is null" className="" src={props.image} />
             </div>
           </Col>
@@ -31,14 +42,14 @@ const QuadShowcase = props => {
         <Col lg={true} className="primary-image px-0">
           <img alt="This is null" className="" src="/imgs/500x600.jpeg" />
         </Col>
-        <Col md={6} lg={4}className="sm-0 px-0 pl-md-2">
-          <div  className="secondary-image">
+        <Col md={6} lg={4} className="sm-0 px-0 pl-md-2">
+          <div className="secondary-image">
             <img alt="This is null" className="" src="/imgs/500x600.jpeg" />
           </div>
-          <div  className="secondary-image">
+          <div className="secondary-image">
             <img alt="This is null" className="" src="/imgs/500x600.jpeg" />
           </div>
-          <div  className="secondary-image">
+          <div className="secondary-image">
             <img alt="This is null" className="" src="/imgs/500x600.jpeg" />
           </div>
         </Col>
