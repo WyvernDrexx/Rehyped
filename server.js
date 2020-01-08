d5195110b6ea3817856ef1934924c9aee4bb77a0
Roscrach <lilaprashadsharma@gmail.com>
2020-01-08 13:07:31 +0530

    SSL Added Client Server

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 require("dotenv").config();
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
 let PORT = process.env.PORT || 3000;
 
-if(process.env.NODE_ENV && process.env.NODE_ENV === "production"){
-  PORT = 80;
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+  PORT = 443;
+  var key = fs.readFileSync(
+    "/etc/letsencrypt/live/rehyped.com/privkey1.pem",
+    "utf-8"
+  );
+
+  var certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/rehyped.com/cert1.pem",
+    "utf-8"
+  );
+
+  var ca = fs.readFileSync(
+    "/etc/letsencrypt/live/rehyped.com/chain1.pem",
+    "utf-8"
+  );
+
+  const credentials = {
+    key,
+    certificate,
+    ca
+  };
+  const httpsServer = https.createServer(credentials, app);
+  const expressHttpServer = express();
+
+  expressHttpServer.use((req, res) => {
+    res.redirect("https://www.rehyped.com/");
+  });
+
+  expressHttpServer.listen(80, () => {
+    console.log("Serving HTTP Server on PORT: " + 80);
+  })
+
+  httpsServer.listen(PORT, _ => {
+    console.log("Serving HTTPS Server on PORT: " + PORT);
+  });
+} else {
+  const httpServer = http.createServer(app);
+  httpServer.listen(PORT, _ => {
+    console.log("Serving HTTP Server on PORT: " + PORT);
+  });
 }
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(PORT, _ => {
-  console.log("React Server started on port: " + PORT);
-});
