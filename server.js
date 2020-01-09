d529769802780a9f5f5625b3de456366b269d2a6
Roscrach <lilaprashadsharma@gmail.com>
2020-01-09 09:24:29 +0530

    compression on server.js added

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
+const compression = require("compression");
 
 let PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
-
+app.use(compression())
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
