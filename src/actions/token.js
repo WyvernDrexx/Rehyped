521e36f31c39f0760f456d8d8235a68386e0c698
Roscrach <lilaprashadsharma@gmail.com>
2020-01-25 18:40:04 +0530

    token to be stored on localstorage

--- src/actions/token.js
+++ src/actions/token.js
@@ -1,15 +1,17 @@
 import { SET_TOKEN, REMOVE_TOKEN, GET_TOKEN, VERIFY_TOKEN } from "./types";
-import cookies from "../utils/Cookies";
+// import cookies from "../utils/Cookies";
 import api from "../api";
 
 const setToken = token => {
-  cookies.remove("token");
-  cookies.set("token", token);
+  // cookies.remove("token");
+  // cookies.set("token", token);
+  localStorage.setItem("token", token);
   return { type: SET_TOKEN, payload: token };
 };
 
 const getToken = _ => {
-  const token = cookies.get("token");
+  // const token = cookies.get("token");
+  const token = localStorage.getItem("token");
   return { type: GET_TOKEN, payload: token };
 };
 
@@ -34,7 +36,8 @@ const verifyToken = _ => async (dispatch, getState) => {
 };
 
 const removeToken = _ => {
-  cookies.remove("token");
+  // cookies.remove("token");
+  localStorage.removeItem("token");
   return { type: REMOVE_TOKEN};
 }
 
