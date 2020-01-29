cdd58cdf63fcef0f614e28bf3951345237c27f53
Roscrach <lilaprashadsharma@gmail.com>
2020-01-29 20:14:43 +0530

    small tweak

--- src/components/ProductsList/ListItem.js
+++ src/components/ProductsList/ListItem.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import history from "../../history";
-import Loader from '../Loader';
-import './ProductsList.scss';
+import Loader from "../Loader";
+import "./ProductsList.scss";
 import ComingSoon from "./ComingSoon";
 import { useDispatch } from "react-redux";
 import { showAlert } from "../../actions";
@@ -11,51 +11,55 @@ const ListItem = props => {
   const { index, item } = props;
   const [isLoaded, setIsLoaded] = useState(false);
   const dispatch = useDispatch();
-  
+
   const divIsLoaded = _ => {
     setIsLoaded(true);
   };
 
-  const imagesSrc = process.env.REACT_APP_IMAGES_SRC;;
+  const imagesSrc = process.env.REACT_APP_IMAGES_SRC;
 
-  if(index % 2 === 0){
+  if (item.isComingSoon) {
     return (
       <Col
-      onClick={_ => dispatch(showAlert("Product Coming Soon!", "success"))}
-      key={index}
-      xs={6}
-      sm={4}
-      lg={3}
-      className="product-item user-select-none cursor-pointer mb-2"
-      onLoad={divIsLoaded}
-    >
-      {isLoaded ? null : (
-        <Loader
-          className="absolute-center"
-        />
-      )}
+        onClick={_ => dispatch(showAlert("Product Coming Soon!", "success"))}
+        key={index}
+        xs={6}
+        sm={4}
+        lg={3}
+        className="product-item user-select-none cursor-pointer mb-2"
+        onLoad={divIsLoaded}
+      >
+        {isLoaded ? null : <Loader className="absolute-center" />}
 
-      <Row className={`image-row mx-2 ${isLoaded ? "" : "visibility-hidden"}`}>
-        <img alt="This is null" src={
-          item.image
-          ? `${imagesSrc + item.image}`
-          : `${imagesSrc + ""}`
-        } />
-      <ComingSoon />
-
-      </Row>
-      <Row className={`mx-2 product-details ${isLoaded ? "" : "visibility-hidden"}`}>
-        <div className="w-100 mt-4">
-          <p className="sub-header text-transform-uppercase font-weight-bold text-left">
-            {item.name.length > 23 ? item.name.slice(0, 23) + "..." : item.name}
-          </p>
-          <p className="mt-4 mr-4">
-            <span className="strikethrough sub-header mr-4">₹{item.price}</span>
-      <span className="sub-header">₹{item.discount}</span>
-          </p>
-        </div>
-      </Row>
-    </Col>
+        <Row
+          className={`image-row mx-2 ${isLoaded ? "" : "visibility-hidden"}`}
+        >
+          <img
+            alt="This is null"
+            src={item.image ? `${imagesSrc + item.image}` : `${imagesSrc + ""}`}
+          />
+          <ComingSoon />
+        </Row>
+        <Row
+          className={`mx-2 product-details ${
+            isLoaded ? "" : "visibility-hidden"
+          }`}
+        >
+          <div className="w-100 mt-4">
+            <p className="sub-header text-transform-uppercase font-weight-bold text-left">
+              {item.name.length > 23
+                ? item.name.slice(0, 23) + "..."
+                : item.name}
+            </p>
+            <p className="mt-4 mr-4">
+              <span className="strikethrough sub-header mr-4">
+                ₹{item.price}
+              </span>
+              <span className="sub-header">₹{item.discount}</span>
+            </p>
+          </div>
+        </Row>
+      </Col>
     );
   }
 
@@ -69,26 +73,25 @@ const ListItem = props => {
       className="product-item hover cursor-pointer mb-2"
       onLoad={divIsLoaded}
     >
-      {isLoaded ? null : (
-        <Loader
-          className="absolute-center"
-        />
-      )}
+      {isLoaded ? null : <Loader className="absolute-center" />}
       <Row className={`image-row mx-2 ${isLoaded ? "" : "visibility-hidden"}`}>
-        <img alt="This is null" src={
-          item.image
-          ? `${imagesSrc + item.image}`
-          : `${imagesSrc + ""}`
-        } />
+        <img
+          alt="This is null"
+          src={item.image ? `${imagesSrc + item.image}` : `${imagesSrc + ""}`}
+        />
       </Row>
-      <Row className={`mx-2 product-details ${isLoaded ? "" : "visibility-hidden"}`}>
+      <Row
+        className={`mx-2 product-details ${
+          isLoaded ? "" : "visibility-hidden"
+        }`}
+      >
         <div className="w-100 mt-4">
           <p className="sub-header text-transform-uppercase font-weight-bold text-left">
             {item.name.length > 23 ? item.name.slice(0, 23) + "..." : item.name}
           </p>
           <p className="mt-4 mr-4">
             <span className="strikethrough sub-header mr-4">₹{item.price}</span>
-      <span className="sub-header">₹{item.discount}</span>
+            <span className="sub-header">₹{item.discount}</span>
           </p>
         </div>
       </Row>
