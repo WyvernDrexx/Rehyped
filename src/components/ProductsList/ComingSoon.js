1dc3446e690ef44b8623631af3ea5f8d8905e087
Roscrach <lilaprashadsharma@gmail.com>
2020-01-29 19:17:07 +0530

    Coming Soon Banner added..

---
+++ src/components/ProductsList/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ProductsList.scss';
+
+export default _ => {
+  return (
+    <div className="coming-soon">
+      <div className="banner">
+        <p className="text-center sub-header default-letter-spacing">COMING SOON</p>
+      </div>
+    </div>
+  );
+}
--- src/components/ProductsList/ListItem.js
+++ src/components/ProductsList/ListItem.js
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
-import { Row, Col, Spinner } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import history from "../../history";
 import Loader from '../Loader';
 import './ProductsList.scss';
+import ComingSoon from "./ComingSoon";
+import { useDispatch } from "react-redux";
+import { showAlert } from "../../actions";
+
 const ListItem = props => {
   const { index, item } = props;
   const [isLoaded, setIsLoaded] = useState(false);
-
+  const dispatch = useDispatch();
+  
   const divIsLoaded = _ => {
     setIsLoaded(true);
   };
 
   const imagesSrc = process.env.REACT_APP_IMAGES_SRC;;
 
-  return (
-    <Col
-      onClick={_ => history.push(`/products/${item._id}`)}
+  if(index % 2 === 0){
+    return (
+      <Col
+      onClick={_ => dispatch(showAlert("Product Coming Soon!", "success"))}
       key={index}
       xs={6}
       sm={4}
       lg={3}
-      className="product-item mb-2"
+      className="product-item user-select-none cursor-pointer mb-2"
       onLoad={divIsLoaded}
     >
       {isLoaded ? null : (
@@ -29,6 +35,45 @@ const ListItem = props => {
         />
       )}
 
+      <Row className={`image-row mx-2 ${isLoaded ? "" : "visibility-hidden"}`}>
+        <img alt="This is null" src={
+          item.image
+          ? `${imagesSrc + item.image}`
+          : `${imagesSrc + ""}`
+        } />
+      <ComingSoon />
+
+      </Row>
+      <Row className={`mx-2 product-details ${isLoaded ? "" : "visibility-hidden"}`}>
+        <div className="w-100 mt-4">
+          <p className="sub-header text-transform-uppercase font-weight-bold text-left">
+            {item.name.length > 23 ? item.name.slice(0, 23) + "..." : item.name}
+          </p>
+          <p className="mt-4 mr-4">
+            <span className="strikethrough sub-header mr-4">₹{item.price}</span>
+      <span className="sub-header">₹{item.discount}</span>
+          </p>
+        </div>
+      </Row>
+    </Col>
+    );
+  }
+
+  return (
+    <Col
+      onClick={_ => history.push(`/products/${item._id}`)}
+      key={index}
+      xs={6}
+      sm={4}
+      lg={3}
+      className="product-item hover cursor-pointer mb-2"
+      onLoad={divIsLoaded}
+    >
+      {isLoaded ? null : (
+        <Loader
+          className="absolute-center"
+        />
+      )}
       <Row className={`image-row mx-2 ${isLoaded ? "" : "visibility-hidden"}`}>
         <img alt="This is null" src={
           item.image
