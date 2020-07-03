7827616e71c9112ce548e1c9f1b35959ca1d0d01
theGorkha <lilaprashadsharma@gmail.com>
2020-07-03 09:44:09 +0530

    cache system added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const files = require("serve-static");
+const cache = require("http-cache-middleware");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -8,38 +10,41 @@ const fs = require("fs");
 const compression = require("compression");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
-const favicon = require('serve-favicon');
+const favicon = require("serve-favicon");
+
+const serve = files(path.join(__dirname, "build", "index.html"), {
+  lastModified: false,
+  setHeaders: (res, path) => {
+    res.setHeader("cache-control", "public, no-cache, max-age=604800");
+  },
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 let PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(express.static(path.join(__dirname, "build")));
-app.use(favicon(path.join(__dirname,"build", "favicon.ico")));
-app.use(compression())
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(compression());
+app.use(cache());
+app.use(serve);
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
   PORT = 443;
-  var key = fs.readFileSync(
-    "/etc/letsencrypt/live/rehyped.com/privkey.pem"
-  );
+  var key = fs.readFileSync("/etc/letsencrypt/live/rehyped.com/privkey.pem");
 
-  var cert = fs.readFileSync(
-    "/etc/letsencrypt/live/rehyped.com/cert.pem"
-  );
+  var cert = fs.readFileSync("/etc/letsencrypt/live/rehyped.com/cert.pem");
 
-  var ca = fs.readFileSync(
-    "/etc/letsencrypt/live/rehyped.com/chain.pem"
-  );
+  var ca = fs.readFileSync("/etc/letsencrypt/live/rehyped.com/chain.pem");
 
   const credentials = {
     key,
     cert,
-    ca
+    ca,
   };
   const httpsServer = https.createServer(credentials, app);
   const expressHttpServer = express();
@@ -50,14 +55,14 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
 
   expressHttpServer.listen(80, () => {
     console.log("Serving HTTP Server on PORT: " + 80);
-  })
+  });
 
-  httpsServer.listen(PORT, _ => {
+  httpsServer.listen(PORT, (_) => {
     console.log("Serving HTTPS Server on PORT: " + PORT);
   });
 } else {
   const httpServer = http.createServer(app);
-  httpServer.listen(PORT, _ => {
+  httpServer.listen(PORT, (_) => {
     console.log("Serving HTTP Server on PORT: " + PORT);
   });
 }
